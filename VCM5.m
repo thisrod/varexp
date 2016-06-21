ab6a6470a238530f3f1df09c29f971142c48b2b3
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-06-21 18:21:04 +1000

    refactoring VCM5

--- VCM5.m
+++ VCM5.m
@@ -6,67 +6,63 @@
 
 a0=2;		% coherent amplitude of initial state
 epsilon=1i*1.e-4;                                 %%stabilize matrix
-%%om= [0;0;0];					                  %%diagona0 frequency
+%%om= [0;0;0];					                  %%diagonal frequency
 om= [0;-4];
 %%kap=[0,1,1];                                      %%anharmonic coupling
 kap=[0,1]; 
+R = 8;		% variational components
+isup = 1;		% index into stores - remove
 
 % Derived parameters
 
 n0 = abs(a0)^2;
-av=conj([0,a0]');                               %%combined initia0 vector
+av=conj([0,a0]');                               %%combined initial vector
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-%%	Sets up initia0 integration data va0ues
+%%	Sets up initial integration data values
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 
-dt=0.005;                                        %%time step 
-sigma=.02;                                        %%initia0 standard deviation
-tmax=2.*pi;                                       %%maximum time 
-%%tmax=.1;
-nt=floor(1+tmax/dt);                              %%points in time for dynamics
-dt=tmax/(nt-1);                                   %%corrected time step 
+sigma=.02;                                        %%initial standard deviation
+
+T=2*pi;  h=0.005;		% time axis
+t = h*(0:ceil(T/h));
 modes=1;                                          %%number of modes 
 M=1+modes;                                        %%size of state vector 
 iters=4;                                          %%iterations of ODE solver
 %%itsteep=0;                                        %%steepest descent iterations
-nsup=2;                                           %%number of superposition loops
 nmeasure=4;                                       %%number of measurements
-store=zeros(nt,nsup,nmeasure);
-t=zeros(nt,1);
-exact=zeros(nt,nmeasure);
-for isup=1:nsup;                                  %%loop over superpositions
-N=8*isup;                                         %%number of superpositions
-NM=N*M;                                           %%multi-vector size
+store=zeros(length(t),nsup,nmeasure);
+exact=zeros(length(t),nmeasure);
+NM=R*M;                                           %%multi-vector size
 omexp=eye(M,M);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 %%	Sets up integration vectors
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 
-da=0.0*1i+zeros(M,N);                             %%complex matrix of zeros 
-a = repmat(av,1,N) + sigma*(randn(M,N)+1i*randn(M,N));                          %%Initia0ises amplitudes
-phimask=[ones(1),zeros(modes,1)']'*ones(1,N);
-amask=[zeros(1),ones(modes,1)']'*ones(1,N);
+da=0.0*1i+zeros(M,R);                             %%complex matrix of zeros 
+a = repmat(av,1,R) + sigma*(randn(M,R)+1i*randn(M,R));                          %%Initia0ises amplitudes
+phimask=[ones(1),zeros(modes,1)']'*ones(1,R);
+amask=[zeros(1),ones(modes,1)']'*ones(1,R);
 a=a.*amask;                                       %%Mask phase to zero
-a_0=a;                                            %%Stores initia0 amplitude
+a_0=a;                                            %%Stores initial amplitude
 delt=eye(M,M);
-deltN=eye(N,N);
+deltR=eye(R,R);
 delt(1,1)=0.0;
 hd=ones(1,M);
 hd(1)=0;
-A1=zeros(NM,M,N)+1i;                                %%Initia0ises A matrix 
+A1=zeros(NM,M,R)+1i;                                %%Initia0ises A matrix 
 A=zeros(NM,NM)+1i;                                %%Initia0ises A matrix 
 H=zeros(NM,1)+1i;                                 %%Initia0ises H matrix 
 AD=zeros(NM,NM)+1i;                                %%Initia0ises A matrix 
 HD=zeros(NM,1)+1i;                                 %%Initia0ises H matrix 
 eps=epsilon*eye(NM,NM); 
-nstore=zeros(nt,N);
-wstore=zeros(nt,N);
+nstore=zeros(length(t),R);
+wstore=zeros(length(t),R);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 	Starts loop in time:
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 
-for it=1:nt;                                      %%loop until time tmax
+for it=1:length(t);                                      %%loop until time T
   if it>1                                         %%If first time, initia0ize
     a=omexp*a;
     a1=a;                                         %%Store multivector
@@ -77,8 +73,8 @@ for it=1:nt;                                      %%loop until time tmax
         logrho=0.5*(a'*at1+at1'*a);               %%Store log density matrix
         rhomax=max(max(real(logrho)));            %%Get maximum weight
         rho=exp(logrho-rhomax);                   %%Renorma0ize weight
-        rhod=rho.*deltN;
-        for m=1:N;                                %%Count superpositions!
+        rhod=rho.*deltR;
+        for m=1:R;                                %%Count superpositions!
           for k=1:M;                              %%Count modes!
             mk=(m-1)*M+k;
               for l=1:M;                          %%Count modes!	
@@ -102,8 +98,8 @@ h2= rho(m,:).*at(k,:).*(h2w+0.5*h2k);                                        %%H
         end;
         A=reshape(A1,NM,NM);
         %%B=A'; C=B*A; D=diag(B);
-        vec1=vec1+(A+eps)\(-1i*H*dt/2-A*vec1); 
-       da=reshape(vec1,M,N);                      %%Reshape to matrix     
+        vec1=vec1+(A+eps)\(-1i*H*h/2-A*vec1); 
+       da=reshape(vec1,M,R);                      %%Reshape to matrix     
        a=a1+da;
     end;                                          %%end iterations
     a=omexp*(a1+2.*da); 
@@ -117,7 +113,7 @@ h2= rho(m,:).*at(k,:).*(h2w+0.5*h2k);                                        %%H
   rho=0.5*(a'*at1+at1'*a);
   rho=exp(rho);                                   %%Renorma0ize weight
   rhosum=real(sum(sum(rho)));
-  for m=1:N;                                      %%Count superpositions!
+  for m=1:R;                                      %%Count superpositions!
     nstore(it,m)=abs(a(2,m))^2;
     wstore(it,m)=real(rho(m,m));                                    
     store(it,isup,1)=store(it,isup,1)+sum(real(rho(m,:).*a(2,:)));	
@@ -128,7 +124,7 @@ h2= rho(m,:).*at(k,:).*(h2w+0.5*h2k);                                        %%H
   store(it,isup,2)=store(it,isup,2)/rhosum;
   store(it,isup,3)=store(it,isup,3)/rhosum;
   store(it,isup,4)=rhosum;
-  t(it)=(it-1)*dt;                                %%Store t-coordinate
+%  t(it)=(it-1)*h;                                %%Store t-coordinate
   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 %% 	Ca0culates exact observables for initia0 coherent state case; assumes
@@ -140,7 +136,6 @@ exact(it,2)=imag(ax);
 exact(it,3)=real(n0); 
 
 end;                                              %%end time loop
-end;						                      %%end superposition loop
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 %%        Output section - gives numerica0 output data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
@@ -187,7 +182,7 @@ figure(5);
 set(gca,'FontSize',fs);
 plot(t,nstore);                           %%Plot max_N
 xlabel('t ','FontSize',fs);
-ylabel('N_va0ues','FontSize',fs);
+ylabel('N_values','FontSize',fs);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 
 figure(6);
