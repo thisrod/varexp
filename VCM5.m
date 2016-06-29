4c625598b715f337d652fcc6b411303862a67177
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-06-29 15:26:35 +1000

    Removed Peter's code

--- VCM5.m
+++ VCM5.m
@@ -16,21 +16,12 @@ nhn = nhn-4*(0:N)';
 a0=2;		% coherent amplitude of initial state
 c0 = nq*evan(a0, 'even');	% expansion over number states
 epsilon=1i*1.e-4;                                 %%stabilize matrix
-%%om= [0;0;0];					                  %%diagonal frequency
-om= [0;-4];
-%%kap=[0,1,1];                                      %%anharmonic coupling
-kap=[0,1]; 
-% kap = kap*0;
+
 R = 16;		% variational components
 
 % Derived parameters
 
 n0 = abs(a0)^2;
-av=conj([0,a0]');                               %%combined initial veqeor
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-%%	Sets up initial integration data values
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
 
 sigma=.02;                                        %%initial standard deviation
 z = zeros(2*R, 1);
@@ -38,8 +29,7 @@ c = 0;		% get rid of this once loop is refactored
 
 T=2*pi;  h=0.005;		% time axis
 t = h*(0:ceil(T/h));
-modes=1;                                          %%number of modes 
-M=1+modes;                                        %%size of state veqeor 
+
 iters=4;                                          %%iterations of ODE solver
 
 % data to plot
@@ -48,117 +38,31 @@ two = one;  three = one;  four = one;
 zdiscrep = one.o; adiscrep = one.o; hdiscrep = one.o; qdiscrep = one.o;  
 csize = one;  qsize = one;
 
-nstore =zeros(length(t),R);
 
-NM=R*M;                                           %%multi-veqeor size
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-%%	Sets up integration veqeors
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
+zp = [zeros(R,1);  a0 + sigma*randn(R,2)*[1; 1i] ];	% draw initial ensemble
 
-da=0.0*1i+zeros(M,R);                             %%complex matrix of zeros 
-a = repmat(av,1,R) + sigma*(randn(M,R)+1i*randn(M,R));                          %%Initia0ises amplitudes
-	zp = a.';  zp = zp(:);
+%% Integration loop
 
-phimask=[ones(1),zeros(modes,1)']'*ones(1,R);
-amask=[zeros(1),ones(modes,1)']'*ones(1,R);
-a=a.*amask;                                       %%Mask phase to zero
-a_0=a;                                            %%Stores initial amplitude
-delt=eye(M,M);
-deltR=eye(R,R);
-delt(1,1)=0.0;
-hd=ones(1,M);
-hd(1)=0;
-A1=zeros(NM,M,R)+1i;                                %%Initia0ises A matrix 
-A=zeros(NM,NM)+1i;                                %%Initia0ises A matrix 
-H=zeros(NM,1)+1i;                                 %%Initia0ises H matrix 
-AD=zeros(NM,NM)+1i;                                %%Initia0ises A matrix 
-HD=zeros(NM,1)+1i;                                 %%Initia0ises H matrix 
-eps=epsilon*eye(NM,NM); 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% 	Starts loop in time:
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-
-for it=1:length(t);                                      %%loop until time T
-  if it>1                                         %%If first time, initia0ize
+for it=1:length(t) 
+if it>1                                         %%If first time, initia0ize
 	zph = zp;
-    a1=a;                                         %%Store multiveqeor
-    vec1=zeros(NM,1);
-    dzt = zeros(2*R,1);  dzp = dzt;	% Tychonov and Peter
-    for iter=1:iters;                             %%iteration loop for ODE
-        at=a.*amask+phimask;                      %%Store eigenva0ues
-        at1=at+phimask;                           %%Store inner produqe terms
-        logrho=0.5*(a'*at1+at1'*a);               %%Store log density matrix
-        rhomax=max(max(real(logrho)));            %%Get maximum weight
-        rho=exp(logrho-rhomax);                   %%Renorma0ize weight
-        rhod=rho.*deltR;
-        for m=1:R;                                %%Count superpositions!
-          for k=1:M;                              %%Count modes!
-            mk=(m-1)*M+k;
-              for l=1:M;                          %%Count modes!	
-                A1(mk,l,:)=rho(m,:).*(delt(k,l)+at(k,:)*at(l,m)');
-              end;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% 	Ca0culate Hamiltonian for anharmonic oscillator
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-   
-h1=hd(k)*rho(m,:).*a(k,:).*(om(k)+kap(k)*a(k,m)'*a(k,:));            %%Commutator term
-h2w= (a(:,m))'*diag(om)*a;	
-h2k= (a(:,m).^2)'*diag(kap)*(a.^2);	
-h2= rho(m,:).*at(k,:).*(h2w+0.5*h2k);                                        %%Hamiltonian term
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% 	End problem-specific code
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/ 	
-              H(mk)=sum(h1+h2);
-                  
-          end;
-        end;
-        A=reshape(A1,NM,NM);
-        %%B=A'; C=B*A; D=diag(B);
-        vec1=vec1+(A+eps)\(-1i*H*h/2-A*vec1); 
-       da=reshape(vec1,M,R);                      %%Reshape to matrix
-       	dza = 2*da.';  dza = dza(:);
-       a=a1+da;
-
-	
-	% independent propagation
-	c = sqrt(sum(abs(nq*evan(zph)).^2));
-	w = zph;  w(1:R) = w(1:R) - log(max(c));
-	ensemble = nq*evan(w);
-	Dq = [ensemble, ndqr*evan(w)];
-	AA = Dq'*Dq;
-	Hq = nhn.*sum(ensemble,2);
-	HH = Dq'*Hq;
-	dzt = dzt + [Dq; sqrt(epsilon)*eye(2*R)] \ [-1i*Hq*h/2-Dq*dzt; zeros(2*R,1)];
-	dzp = dzp + (AA+epsilon*eye(2*R))\(-1i*HH*h-AA*dzp);
-	zph = zp + dzp/2;
-	
-    end;                                          %%end iterations
+	dzt = zeros(2*R,1);  dzp = dzt;	% Tychonov and Peter
+	for iter=1:iters
+		c = sqrt(sum(abs(nq*evan(zph)).^2));
+		w = zph;  w(1:R) = w(1:R) - log(max(c));
+		ensemble = nq*evan(w);
+		Dq = [ensemble, ndqr*evan(w)];
+		AA = Dq'*Dq;
+		Hq = nhn.*sum(ensemble,2);
+		HH = Dq'*Hq;
+		dzt = dzt + [Dq; sqrt(epsilon)*eye(2*R)] \ [-1i*Hq*h/2-Dq*dzt; zeros(2*R,1)];
+		dzp = dzp + (AA+epsilon*eye(2*R))\(-1i*HH*h-AA*dzp);
+		zph = zp + dzp/2;
+	end
     	zp = zp + dzp;
-    a=a1+2.*da;
-	z = a.';  z = z(:);
-	c = sqrt(sum(abs(nq*evan(z)).^2));
-    shuffle = [1:2:2*R 2:2:2*R];
-    zdiscrep(it) = discrepency(dza, dzp);
-    adiscrep(it) = discrepency(A(shuffle, shuffle), AA, 'fro');
-    hdiscrep(it) = discrepency(H(shuffle), HH, 'fro');
-    orank(it) = rank(Dq);   rrank(it) = rank([Dq; sqrt(epsilon)*eye(2*R)]);  
-  end;                                            %%end if first time
+	orank(it) = rank(Dq);   rrank(it) = rank([Dq; sqrt(epsilon)*eye(2*R)]);  
+end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-%% 	Ca0culates observables
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-
-	qdiscrep(it) = discrepency(sum(nq*evan(z), 2), sum(nq*evan(zp), 2));
-
-	% brackets
-	qo = sum(nq*evan(z), 2);
-	qsize.p(it) = norm(qo);
-	alphao = qo'*aop*qo/qsize.p(it)^2;
-	one.p(it) = real(alphao);  two.p(it) = imag(alphao);
-	three.p(it) = sum(abs(qo).^2.*(0:N)')/qsize.p(it)^2;
-	csize.p(it) = norm(c);
-	
 	% independent brackets
 	qo = sum(nq*evan(zp), 2);
 	qsize.o(it) = norm(qo);
@@ -166,49 +70,17 @@ h2= rho(m,:).*at(k,:).*(h2w+0.5*h2k);                                        %%H
 	one.o(it) = real(alphao);  two.o(it) = imag(alphao);
 	three.o(it) = sum(abs(qo).^2.*(0:N)')/qsize.o(it)^2;
 	csize.o(it) = norm(c);
-	
-
-	
-	% Peter for comparison
-	
-  at1=a.*amask+2*phimask;                         %%Store eigenva0ues
-  rho=0.5*(a'*at1+at1'*a);
-  rho=exp(rho);                                   %%Renorma0ize weight
-  rhosum=real(sum(sum(rho)));
-  for m=1:R;                                      %%Count superpositions!
-    nstore(it,m)=abs(a(2,m))^2;
-    one.o(it)=one.o(it)+sum(real(rho(m,:).*a(2,:)));	
-    two.o(it)=two.o(it)+sum(real(rho(m,:).*a(2,:)/1i));
-    three.o(it)=three.o(it)+real(sum(rho(m,:).*a(2,:))*a(2,m)');           	   
-  end;
-  one.o(it)=one.o(it)/rhosum;
-  two.o(it)=two.o(it)/rhosum;
-  three.o(it)=three.o(it)/rhosum;
-  four.o(it)=rhosum;
   
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-%% 	Ca0culates exaqe observables for initia0 coherent state case; assumes
-%%  a single anharmonic oscillator with nonlinear coupling and detuning
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
+	% Exact values for comparison
+	
 qe = exp(-1i*nhn*t(it)).*c0;
 alphae = qe'*aop*qe;
 one.c(it)=real(alphae);
 two.c(it)=imag(alphae);
 three.c(it)=real(n0); 
 
-end;                                              %%end time loop
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
-%%        Output seqeion - gives numerica0 output data
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/
+end
 
-
-figure
-plot(t,zdiscrep,'-k', t,adiscrep,':k',  t,hdiscrep,'--k');
-xlabel t, ylabel discrepency, legend z A H
-
-figure
-plot(t,qdiscrep,'-k');
-xlabel t, ylabel 'state discrepency'
 
 figure
 plot(t,orank,':k',t,rrank,'-k');
